bc8433168806c329651c65b7fe8608bc80290952
yousseflaribi <yousseflaribi10@gmail.com>
2024-12-23 15:03:23 +0100

    first commit

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,14 @@
+import { NextUIProvider } from "@nextui-org/react";
+import React,{ ReactNode } from "react";
+
+
+export default function Providers({
+    children,
+}:{
+    children: ReactNode;
+}
+) {
+  return (
+    <NextUIProvider>{children}</NextUIProvider>
+  )
+}
